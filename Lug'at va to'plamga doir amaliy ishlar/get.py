c54fd529845003703cf62dc737ab6e037c5b58e2
Xasan Kattayev <xasankattayev247@gmail.com>
2024-04-11 12:28:27 +0500

    yangi amaliy ish qo'shildi

---
+++ Lug'at va to'plamga doir amaliy ishlar/get.py
@@ -0,0 +1,14 @@
+# avtomobil={'model':'spark', 'rang':'oq', 'yil':'2010'}
+# print(avtomobil['rang'])
+#----------------------------------------------------------------
+# talaba={'ism':'Salimov Olim', 'yosh':25, 't_yil': '2000'}
+# print(f"{talaba['ism'].title()},\
+#       {talaba['t_yil']}-yilda tug'ilgan,\
+#       {talaba['yosh']} yoshda")
+#----------------------------------------------------------------
+# talaba={'ism':'Salimov Olim', 'yosh':25, 't_yil': '2000'}
+# talaba['kurs']=4
+# talaba['fakultet']='Axborot texnologiyalar'
+# print(talaba)
+#----------------------------------------------------------------
+
