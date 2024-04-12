dfe2db4e3aa804a951502cd4368513bb695c9376
Xasan Kattayev <xasankattayev247@gmail.com>
2024-04-12 12:54:41 +0500

    misollar qo'shildi

---
+++ To'plamlar/toplam.py
@@ -0,0 +1,36 @@
+# ranglar={'oq', 'qora', 'qizil'}
+# ranglar.add('kul rang')
+# ranglar.update(['sariq', 'malla', 'yashil'])
+# print (ranglar)
+#----------------------------------------------------------------
+# set1={10,20,30,40,50}
+# set2={30,40,50,60,70}
+# set3=set1-set2
+# print(set3)
+#----------------------------------------------------------------
+# set1={10,20,30,40,50}
+# set2={30,40,50,60,70}
+# set3=set1^set2
+
+# print(set3)
+#----------------------------------------------------------------
+# bozorlik={'choy', 'non', 'kartoshka', 'tuxum', 'sut'}
+# mahsulotlar={'non', 'sut', 'tuxum', 'olma', 'un', 'tuz'}
+# list=bozorlik&mahsulotlar
+# print(list)
+#----------------------------------------------------------------
+# bozorlik={'choy', 'non', 'kartoshka', 'tuxum', 'sut'}
+# mahsulotlar={'non', 'sut', 'tuxum', 'olma', 'un', 'tuz'}
+# print("Do'konda mavjud bo'lmagan mahsulotlar ro'yxati")
+
+# list = bozorlik - mahsulotlar
+# print(list)
+#----------------------------------------------------------------
+# bozorlik={'choy', 'non', 'kartoshka', 'tuxum', 'sut'}
+# mahsulotlar={'non', 'sut', 'tuxum', 'olma', 'un', 'tuz'}
+
+
+# mahsulotlar.update(['choy', 'kartoshka'])
+# print(mahsulotlar)
+#----------------------------------------------------------------
+
