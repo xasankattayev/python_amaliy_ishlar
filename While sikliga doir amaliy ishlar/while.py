729b1022757964cd5026397cba1d902211ea9f60
Xasan Kattayev <xasankattayev247@gmail.com>
2024-04-13 21:51:19 +0500

    yangi misollar qo'shildi

---
+++ While sikliga doir amaliy ishlar/while.py
@@ -0,0 +1,41 @@
+# print("Kiritilgan sonning kvadratini hisoblaydi:")
+# savol="Istalgan son kiriting"
+# savol+="(To'xtatish uchun 'exit' deb yozing):"
+# qiymat=''
+# while qiymat !='exit':
+#     qiymat =input(savol)
+#     if qiymat != 'exit':
+#         print(float(qiymat)**2)
+#-----------------------------------------------------------
+# son=1
+# while son<=5:
+#     print (son, end=' ')
+#     son=son+1
+# print("Dastur tugadi")
+#---------------------------------------------------------
+# print("Kitoblarning ro'yxatini yozing")
+# kitob="Siz yoqtirgan kitoblarni kiriting: "
+# kitob1=''
+# while kitob1 !='stop':
+#     kitob1 =input(kitob)
+#     if kitob1 !='stop':
+#         print(kitob1.title())
+#---------------------------------------------------------
+# print("Muzeyga kirish uchun chipta narxlar quyidagicha")
+# yosh="Yoshingizni kiriting: "
+# yosh1=''
+# while yosh1 !='stop':
+#     yosh1 =int(input(yosh))
+#     if yosh1 !='stop':
+#         
+#         if yosh1<=7:
+#             print("Muzeyga kirish uchun 2 000 so'm")
+#         elif 7<yosh1<=18:
+#             print("Muzeyga kirish uchun 3 000 so'm")
+#         elif 18<yosh1<=65:
+#             print("Muzeyga kirish uchun 10 000 so'm")
+#         elif yosh1>65:
+#             print("Muzeyga kirish bepul")
+# print('Yoshingizni qayta kiriting')
+#----------------------------------------------------------
+
