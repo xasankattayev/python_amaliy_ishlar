54a92625d6f01b1ba6aec5e3195269f0ea6bae41
Xasan Kattayev <xasankattayev247@gmail.com>
2024-04-22 22:01:42 +0500

    yangi misollar qo'shildi

---
+++ Modulga doir amaliy ishlar/lambda.py
@@ -0,0 +1,24 @@
+# import math
+# son=lambda x : x*10
+# print(son(3))
+#-------------------------------------------------
+# import math
+# son=lambda x,y : x+y
+
+# print(son(10,4))
+#-------------------------------------------------
+# import random as r
+# sonlar=r.sample(range(1000), 10)
+# print(sonlar)
+#-------------------------------------------------
+# sonlar=[5,2,8,7,25,63]
+# kvadrat=list(map(lambda a: a*a,sonlar))
+# print(kvadrat)
+#-------------------------------------------------
+# import random as r
+# sonlar=r.sample(range(1000),15)
+# toq_sonlar=list(filter(lambda son: son%2==1,sonlar))
+
+# print(sonlar)
+# print(toq_sonlar)
+#------------------------------------------------------
