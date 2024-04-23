0c0b36136793c131fd2e5f958f330758807787cd
Xasan Kattayev <xasankattayev247@gmail.com>
2024-04-23 22:37:29 +0500

    yangi misollar

---
+++ OOP ga doira amaliy ishlar/oop.py
@@ -0,0 +1,55 @@
+# class User:
+#     def __init__(self, ism, familiya, email, parol,tugilgan_joy):
+#         self.ism = ism
+#         self.familiya = familiya
+#         self.email = email
+#         self.parol = parol
+#         self.tugilgan_joy = tugilgan_joy
+        
+#     def familiya(self):
+#         return self.familiya
+#     def tugilgan_joy(self):
+        
+#         return self.tugilgan_joy
+
+
+
+
+
+
+# user1=User('xasan', 'hakimov', 'xasan@mail.ru', '1236547xasan','termiz')
+
+# print(user1.ism)
+# print(user1.parol)
+# print(user1.familiya)
+#----------------------------------------------------------------
+# class User:
+#     def __init__(self, foydalanuvchi, ism, email, parol):
+#         self.foydalanuvchi = foydalanuvchi
+#         self.ism = ism
+#         self.email = email
+#         self.parol = parol
+        
+        
+#     # def familiya(self):
+#     #     return self.familiya
+#     def get_info(self):
+#         print(f"Foydalanuvchi: {self.foydalanuvchi}, ism: {self.ism}, email: {self.email}" )
+
+
+# user1=User('xasan1996', 'xasan', 'xasan@mail.ru', '1236547xasan')
+
+# print(user1.get_info())
+#-----------------------------------------------------------------------------
+class User:
+
+    def get_ism(self):
+        return self.ism
+    def get_familiya(self):
+        return self.familiya
+    
+    def get_malumot(self):
+        return (f"{self.ism} {self.familiya}")
+
+talaba1=User ('sdsds','hakimov')
+print(talaba1.get_malumot())
