17b27237dc9d8ce5047c5279c737f9dc3cdc70e3
Xasan Kattayev <xasankattayev247@gmail.com>
2024-04-29 22:48:11 +0500

    yangi misol qo'shildi

---
+++ Fayllar bilan ishlash/fayl/fayl.py
@@ -0,0 +1,60 @@
+# with open ('pi.txt') as fayl:
+#     pi=fayl.read
+#     print(pi)
+#     fayl.close()
+# file=open('pi.txt')
+# pi=file.read()
+# print(pi)
+# file.close()
+#-----------------------------------------------------------
+# with open('text/matn.txt') as fayl:
+#     matn=fayl.read()
+#     print(matn)
+#     fayl.close()
+#-----------------------------------------------------------
+# faylname='text/matn.txt'
+# with open(faylname) as fayl:
+#     nomlar=fayl.readlines()
+#     print(nomlar)
+#-----------------------------------------------------------
+# faylnomi='kolektivlar.txt'
+# with open(faylnomi,'w') as fayl:
+#     fayl.write("Assalomu alaykum")
+
+# faylnomi='yangi_file.txt'
+# ism="Xasan Kattayev"
+# tyil=1996
+# with open(faylnomi, 'w') as fayl:
+#     fayl.write(ism+'\n')
+#     fayl.write(str(tyil)+'\n')
+# #------------------------------------------------------------
+# with open(faylnomi,'a') as fayl:
+#     fayl.write('Xusan Kattayev'+'\n')
+#     fayl.write('1996'+'\n')
+#----------------------------------------------------------
+# with open('pi.txt') as fayl:
+#     pi=fayl.read()
+#     print(pi)
+#     fayl.close()
+#----------------------------------------------------------
+# bdate = '31122000'
+# pi=3.14151715874622148
+# print(bdate in pi)
+
+# pi = float(pi) # matnni float (o'nlik) songa o'tkazamiz
+
+# with open('amaliyot/pi_float.dat','wb') as file:
+#     pickle.dump(pi,file)
+#----------------------------------------------------------------
+# faylnomi='yangi_file.txt'
+# ism=input("Ism familyangizni kiriting: ")
+# tyil=int(input("Tug'ilgan yilingizni kiriting:"))
+# with open(faylnomi, 'a') as fayl:
+#     fayl.write(ism+'\n')
+#     fayl.write(str(tyil)+'\n')
+
+    
+    
+
+
+
