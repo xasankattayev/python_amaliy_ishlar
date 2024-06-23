211fab741c7e3913a2a261ca01d62ee94b19b011
Xasan Kattayev <xasankattayev247@gmail.com>
2024-06-23 22:17:44 +0500

    Update uyga_vazifa.py

--- takrorlash vazifa/uyga_vazifa.py
+++ takrorlash vazifa/uyga_vazifa.py
@@ -1,13 +1,24 @@
-print("Hurmatli foydalanuvchi Mexmonxona narxlari bilan tanishing")
 
-qavat=int(input("HUrmatli mijoz qavatni kiriting: "))
+# 1-Amaliy vazifa
+# print("Hurmatli foydalanuvchi Mexmonxona narxlari bilan tanishing")
 
-if 1 <= qavat <= 4:
-    print(f"{qavat} - qavat narxi 300 $")
-elif 5 <= qavat <= 8:
-    print(f"{qavat} - qavat narxi 600 $")
-elif 8 < qavat <= 10:
-    print(f"{qavat} - qavat narxi 1000 $")
+# qavat=int(input("HUrmatli mijoz qavatni kiriting: "))
 
-elif qavat > 10:
-    print("Hurmatli mijoz Siz kiritgan qavat mavjud emas")
+# if 1 <= qavat <= 4:
+#     print(f"{qavat} - qavat narxi 300 $")
+# elif 5 <= qavat <= 8:
+#     print(f"{qavat} - qavat narxi 600 $")
+# elif 8 < qavat <= 10:
+#     print(f"{qavat} - qavat narxi 1000 $")
+
+# elif qavat > 10:
+#     print("Hurmatli mijoz Siz kiritgan qavat mavjud emas")
+
+#---------------------------
+# 2-Amaliy vazifa
+# son1=0
+# for i in range(1,21):
+#     if i%2 == 0:
+#         son1+=i
+# print(son1)
+
